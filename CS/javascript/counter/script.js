2419d52deeb86f298161b9b5ea57f9ee5be3fcba
Joshy-man <joshua.ware@hgs.hiddengeniusproject.org>
2024-09-14 12:17:30 -0700

    Update script.js

--- CS/javascript/counter/script.js
+++ CS/javascript/counter/script.js
@@ -1,4 +1,4 @@
-function counter() 
+function counter() {
     // Write your code below. Hint: you can finish it in about 3-5 lines.
     // create variable to store change.
     counter_number = document.getElementById('counter-number');
