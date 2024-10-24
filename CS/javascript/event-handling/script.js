34c5d5aee725e12d49e1bd9ec3847bde0df27103
Joshy-man <joshua.ware@hgs.hiddengeniusproject.org>
2024-10-23 17:46:27 -0700

    Update script.js

--- CS/javascript/event-handling/script.js
+++ CS/javascript/event-handling/script.js
@@ -1,3 +1,11 @@
-function eventHandling() {
-    // Your code here.
+// Find the button by it's ID and use the addEventListener()
+// method to make the button listen for button clicks
+document.getElementById("myButton").addEventListener("click", myBtn);
+let count = 0;
+let countEl = document.getElementById("myCount"); // Make sure this ID matches your HTML
+
+function myBtn() {
+    count = count + 1;
+    countEl.innerText = count;
+
 }
