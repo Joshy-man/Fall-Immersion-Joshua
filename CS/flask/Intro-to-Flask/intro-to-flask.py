dca17a075250f8851b5494d0b84bd952e37dbc95
Joshy-man <joshua.ware@hgs.hiddengeniusproject.org>
2024-12-04 18:10:45 -0800

    Update intro-to-flask.py

--- CS/flask/Intro-to-Flask/intro-to-flask.py
+++ CS/flask/Intro-to-Flask/intro-to-flask.py
@@ -1,1 +1,12 @@
-# instruction will below. 
+#Import the flask class to the flask library
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Home Page"
+
+
+if __name__ =='__main__':
+    app.run(debug =True)# instruction will below. 
